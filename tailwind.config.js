0feb4a3a259984cf25c19b1ab063fd2de3b040c3
Ibson Barboza de Oliveira <128850394+IbsonBarboza@users.noreply.github.com>
2024-02-01 22:03:31 -0500

    html

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        StrongCyan: 'hsl(171, 66%, 44%)',
+        LightBlue: 'hsl(233, 100%, 69%)',
+        DarkGrayishBlue: 'hsl(210, 10%, 33%)',
+        GrayishBlue: 'hsl(201, 11%, 66%)',
+      },
+    },
+  },
+  plugins: [],
+}
